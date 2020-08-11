9b10ed6c7d42142808198060c6eb3ceb70fa8247
Willem Meijer <willem1998m@hotmail.com>
2020-08-11 13:31:30 +0200

    clean up

--- Assets/Mods/NMTechSupport/Scripts/InteractableButton.cs
+++ Assets/Mods/NMTechSupport/Scripts/InteractableButton.cs
@@ -1,8 +1,4 @@
-﻿using NUnit.Framework.Constraints;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System;
 using UnityEngine;
 
 namespace WillemMeijer.NMTechSupport
