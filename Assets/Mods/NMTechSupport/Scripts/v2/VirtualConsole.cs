27683787c2de191f1be3412aca3b457ca535267e
Willem Meijer <willem1998m@hotmail.com>
2022-08-13 20:37:11 +0100

    formatting v2

--- Assets/Mods/NMTechSupport/Scripts/v2/VirtualConsole.cs
+++ Assets/Mods/NMTechSupport/Scripts/v2/VirtualConsole.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public class VirtualConsole: MonoBehaviour, IButtonSubscriber {
+public class VirtualConsole : MonoBehaviour, IButtonSubscriber
+{
     [SerializeField] private TextMeshBox textBox;
     [SerializeField] private TextMesh text;
-    [SerializeField] private int maxMessageCount; 
+    [SerializeField] private int maxMessageCount;
     [SerializeField] private ButtonPublisher buttonPublisher;
     [SerializeField] private GameObject screenDimmedOverlay;
 
     [Space]
-    [SerializeField] private string messageFormat; 
+    [SerializeField] private string messageFormat;
     [SerializeField] private string selectedOptionFormat;
     [SerializeField] private string unselectedOptionFormat;
 
@@ -19,25 +20,30 @@ public class VirtualConsole: MonoBehaviour, IButtonSubscriber {
     private Dictionary<int, int> shuffledOptionMap = new Dictionary<int, int>();
     private int currentOption = 0;
 
-    public void Start() {
+    public void Start()
+    {
         buttonPublisher.Subscribe(this);
         Clear();
     }
 
-    public void ToggleDim(bool isEnabled) {
+    public void ToggleDim(bool isEnabled)
+    {
         this.screenDimmedOverlay.SetActive(isEnabled);
     }
 
-    public int GetCurrentOption() {
+    public int GetCurrentOption()
+    {
         return this.shuffledOptionMap[this.currentOption];
     }
-    
-    public void Write(string message, string[] options, bool isShuffled) {
+
+    public void Write(string message, string[] options, bool isShuffled)
+    {
         WriteMessage(message);
         WriteOptions(options, isShuffled);
     }
 
-    public void WriteMessage(string message) {
+    public void WriteMessage(string message)
+    {
         message = string.Format(messageFormat, message);
         messageMemento.AddLast(message);
         if (messageMemento.Count >= this.maxMessageCount)
@@ -45,47 +51,55 @@ public class VirtualConsole: MonoBehaviour, IButtonSubscriber {
         RefreshMessages();
     }
 
-    public void WriteOptions(string[] options, bool isShuffled) {
+    public void WriteOptions(string[] options, bool isShuffled)
+    {
         this.options = options;
         currentOption = 0;
         shuffledOptionMap.Clear();
-        if (isShuffled) {
-            int[] shuffledIndices = new int[options.Length]; 
-            for (int i = 0; i < options.Length; i++) 
+        if (isShuffled)
+        {
+            int[] shuffledIndices = new int[options.Length];
+            for (int i = 0; i < options.Length; i++)
                 shuffledIndices[i] = i;
             shuffledIndices = shuffledIndices.Shuffle();
             for (int i = 0; i < options.Length; i++)
                 shuffledOptionMap.Add(i, shuffledIndices[i]);
         }
-        else {
-            for (int i = 0; i < options.Length; i++) 
+        else
+        {
+            for (int i = 0; i < options.Length; i++)
                 shuffledOptionMap.Add(i, i);
         }
         RefreshOptions();
     }
 
-    public void Refresh() {
+    public void Refresh()
+    {
         RefreshMessages();
         RefreshOptions();
     }
 
-    public void RefreshMessages() {
+    public void RefreshMessages()
+    {
         textBox.SetText(messageMemento.Join("\n"));
     }
 
-    public void RefreshOptions() {
+    public void RefreshOptions()
+    {
         string text = this.text.text;
-        for (int i = 0; i < options.Length; i++) {
+        for (int i = 0; i < options.Length; i++)
+        {
             string opt = options[this.shuffledOptionMap[i]];
             opt = i == currentOption
                 ? string.Format(selectedOptionFormat, opt)
                 : string.Format(unselectedOptionFormat, opt);
-            text= string.Format("{0}\n{1}", text, opt);
+            text = string.Format("{0}\n{1}", text, opt);
         }
         textBox.SetText(text);
     }
 
-    public void Clear() {
+    public void Clear()
+    {
         textBox.SetText("");
         messageMemento.Clear();
         shuffledOptionMap.Clear();
@@ -93,13 +107,13 @@ public class VirtualConsole: MonoBehaviour, IButtonSubscriber {
     }
 
     public void OnOkButtonClicked()
-    {   
+    {
         RefreshMessages();
     }
 
     public void OnUpButtonClicked()
     {
-        currentOption = options.Length > 0 
+        currentOption = options.Length > 0
             ? currentOption = (currentOption + options.Length - 1) % options.Length
             : 0;
         Refresh();
